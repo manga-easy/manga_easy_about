2f40387cfb60d2afeafcaa3cf6453717d48411e3
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-05-04 20:40:29 -0300

    changed icons

--- lib/src/feactures/domain/entities/team_role_enum.dart
+++ lib/src/feactures/domain/entities/team_role_enum.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 
 enum TeamRoleEnum {
-  dev(text: 'Dev', icon: Icons.abc, color: Color.fromARGB(255, 218, 184, 13)),
-  design(text: 'Design', icon: Icons.abc, color: Color(0xffa2e2cf)),
-  redactor(text: 'Redator', icon: Icons.abc, color: Color(0xff33c08b)),
-  support(text: 'Suporte', icon: Icons.abc, color: Color(0xff2dff5d)),
-  manager(text: 'Gerente', icon: Icons.abc, color: Color(0xffba003e));
+  dev(text: 'Dev', icon: Icons.settings, color: Color.fromARGB(255, 218, 184, 13)),
+  design(text: 'Design', icon: Icons.brush, color: Color.fromARGB(255, 129, 181, 165)),
+  redactor(text: 'Redator', icon: Icons.edit, color: Color(0xff33c08b)),
+  support(text: 'Suporte', icon: Icons.chat_rounded, color: Color(0xff2dff5d)),
+  manager(text: 'Gerente', icon: Icons.person, color: Color(0xffba003e));
 
   final String text;
   final IconData icon;
