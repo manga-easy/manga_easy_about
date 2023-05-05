9f3832dd22adc4a9a940766ba616097509fdb919
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-05-05 15:25:33 -0300

    changed order

--- lib/src/feactures/domain/entities/team_role_enum.dart
+++ lib/src/feactures/domain/entities/team_role_enum.dart
@@ -1,15 +1,35 @@
 import 'package:flutter/material.dart';
 
 enum TeamRoleEnum {
-  dev(text: 'Dev', icon: Icons.settings, color: Color.fromARGB(255, 218, 184, 13)),
-  design(text: 'Design', icon: Icons.brush, color: Color.fromARGB(255, 129, 181, 165)),
-  redactor(text: 'Redator', icon: Icons.edit, color: Color(0xff33c08b)),
-  support(text: 'Suporte', icon: Icons.chat_rounded, color: Color(0xff2dff5d)),
-  manager(text: 'Gerente', icon: Icons.person, color: Color(0xffba003e));
+  dev(
+    text: 'Dev',
+    icon: Icons.settings,
+    color: Color.fromARGB(255, 218, 184, 13),
+  ),
+  manager(
+    text: 'Gerente',
+    icon: Icons.person,
+    color: Color(0xffba003e),
+  ),
+  support(
+    text: 'Suporte',
+    icon: Icons.chat_rounded,
+    color: Color(0xff2dff5d),
+  ),
+  design(
+      text: 'Design',
+      icon: Icons.brush,
+      color: Color.fromARGB(255, 129, 181, 165)),
+  redactor(
+    text: 'Redator',
+    icon: Icons.edit,
+    color: Color(0xff33c08b),
+  );
 
   final String text;
   final IconData icon;
   final Color color;
 
-  const TeamRoleEnum({required this.text, required this.icon, required this.color});
+  const TeamRoleEnum(
+      {required this.text, required this.icon, required this.color});
 }
