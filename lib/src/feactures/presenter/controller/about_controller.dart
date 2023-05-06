a726e6c590ae9edb4eb0e7a6c3d490640cd38241
Cledson Ventura <72049057+cledsonv@users.noreply.github.com>
2023-05-06 11:43:20 -0300

    changed image team

--- lib/src/feactures/presenter/controller/about_controller.dart
+++ lib/src/feactures/presenter/controller/about_controller.dart
@@ -46,76 +46,28 @@ class AboutController extends ChangeNotifier {
 
   List<TeamEntity> team = [
     TeamEntity(
-      name: 'Lucas',
+      name: 'CledsonV',
       color: TeamRoleEnum.dev.color,
       icon: TeamRoleEnum.dev.icon,
       role: TeamRoleEnum.dev.text,
       image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
+          'https://media.discordapp.net/attachments/1104184728535449631/1104417596465500171/R.png?width=344&height=251',
     ),
     TeamEntity(
-      name: 'Cledson',
+      name: 'Lucas CM',
       color: TeamRoleEnum.dev.color,
       icon: TeamRoleEnum.dev.icon,
       role: TeamRoleEnum.dev.text,
       image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
+          'https://media.discordapp.net/attachments/1104184728535449631/1104200454054883459/iu.png?width=375&height=449',
     ),
     TeamEntity(
-      name: 'Bruno',
-      color: TeamRoleEnum.dev.color,
-      icon: TeamRoleEnum.dev.icon,
-      role: TeamRoleEnum.dev.text,
-      image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
-    ),
-    TeamEntity(
-      name: 'Baltros',
-      color: TeamRoleEnum.dev.color,
-      icon: TeamRoleEnum.dev.icon,
-      role: TeamRoleEnum.dev.text,
-      image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
-    ),
-    TeamEntity(
-      name: 'Ricardao',
-      color: TeamRoleEnum.dev.color,
-      icon: TeamRoleEnum.dev.icon,
-      role: TeamRoleEnum.dev.text,
-      image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
-    ),
-    TeamEntity(
-      name: 'Elizabeth',
+      name: 'Skynoshaine',
       color: TeamRoleEnum.manager.color,
       icon: TeamRoleEnum.manager.icon,
       role: TeamRoleEnum.manager.text,
       image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
-    ),
-    TeamEntity(
-      name: 'Sky',
-      color: TeamRoleEnum.manager.color,
-      icon: TeamRoleEnum.manager.icon,
-      role: TeamRoleEnum.manager.text,
-      image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
-    ),
-    TeamEntity(
-      name: 'Luiz',
-      color: TeamRoleEnum.support.color,
-      icon: TeamRoleEnum.support.icon,
-      role: TeamRoleEnum.support.text,
-      image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
-    ),
-    TeamEntity(
-      name: 'Koro',
-      color: TeamRoleEnum.support.color,
-      icon: TeamRoleEnum.support.icon,
-      role: TeamRoleEnum.support.text,
-      image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
+          'https://media.discordapp.net/attachments/1104184728535449631/1104201206898888705/6bff9d15f454afd6dfffbabc0697cdd5.jpg?width=647&height=647',
     ),
     TeamEntity(
       name: 'Darkzy',
@@ -123,15 +75,23 @@ class AboutController extends ChangeNotifier {
       icon: TeamRoleEnum.support.icon,
       role: TeamRoleEnum.support.text,
       image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
+          'https://media.discordapp.net/attachments/1104184728535449631/1104202403412189214/04b242dcf03273f5cd835d2cbb8164f7.jpg?width=656&height=650',
     ),
     TeamEntity(
-      name: 'Theus',
+      name: 'Koro',
       color: TeamRoleEnum.support.color,
       icon: TeamRoleEnum.support.icon,
       role: TeamRoleEnum.support.text,
       image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
+          'https://media.discordapp.net/attachments/1104184728535449631/1104200150529884190/20210626090221_2d701.jpeg?width=359&height=359',
+    ),
+    TeamEntity(
+      name: 'Luiz Henrique',
+      color: TeamRoleEnum.support.color,
+      icon: TeamRoleEnum.support.icon,
+      role: TeamRoleEnum.support.text,
+      image:
+          'https://media.discordapp.net/attachments/1104184728535449631/1104205804850131034/images_-_2022-02-17T223950.637.jpeg?width=665&height=373',
     ),
     TeamEntity(
       name: 'Pato',
@@ -139,7 +99,15 @@ class AboutController extends ChangeNotifier {
       icon: TeamRoleEnum.support.icon,
       role: TeamRoleEnum.support.text,
       image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
+          'https://media.discordapp.net/attachments/1104184728535449631/1104203845162258564/afdb2947aca8e0d6171807d096a186d8.jpg?width=633&height=650',
+    ),
+    TeamEntity(
+      name: 'Theus',
+      color: TeamRoleEnum.support.color,
+      icon: TeamRoleEnum.support.icon,
+      role: TeamRoleEnum.support.text,
+      image:
+          'https://media.discordapp.net/attachments/1104198872449953844/1104200718832910356/IMG_20230505_211847.jpg?width=584&height=584',
     ),
     TeamEntity(
       name: 'Thiago',
@@ -150,28 +118,36 @@ class AboutController extends ChangeNotifier {
           'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
     ),
     TeamEntity(
-      name: 'Nietz',
-      color: TeamRoleEnum.design.color,
-      icon: TeamRoleEnum.design.icon,
-      role: TeamRoleEnum.design.text,
-      image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
-    ),
-    TeamEntity(
       name: 'Anyca',
       color: TeamRoleEnum.design.color,
       icon: TeamRoleEnum.design.icon,
       role: TeamRoleEnum.design.text,
       image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
+          'https://media.discordapp.net/attachments/1104184728535449631/1104201597376004136/d2fd3e37fd0c4ffba11214faade6d5a8.jpg?width=485&height=526',
     ),
     TeamEntity(
-      name: 'Gabiru',
+      name: 'Elizabeth',
+      color: TeamRoleEnum.design.color,
+      icon: TeamRoleEnum.design.icon,
+      role: TeamRoleEnum.design.text,
+      image:
+          'https://media.discordapp.net/attachments/1008262446651297852/1104204488098721912/Bluewing_Comics_style_Young_woman_reading_a_manga_in_a_library__c9d63825-c41a-43c6-bf6c-0a391c2aa181.png?width=400&height=584',
+    ),
+    TeamEntity(
+      name: 'Nietz',
+      color: TeamRoleEnum.design.color,
+      icon: TeamRoleEnum.design.icon,
+      role: TeamRoleEnum.design.text,
+      image:
+          'https://media.discordapp.net/attachments/1104184728535449631/1104196412620681277/image.png?width=463&height=468',
+    ),
+    TeamEntity(
+      name: 'Gahbi',
       color: TeamRoleEnum.redactor.color,
       icon: TeamRoleEnum.redactor.icon,
       role: TeamRoleEnum.redactor.text,
       image:
-          'https://media.discordapp.net/attachments/882615192431771668/1078880185170006056/5d138386cb0ff237d0f65cd6a9905ab4.jpg?width=576&height=650',
+          'https://media.discordapp.net/attachments/1104184728535449631/1104208597325451286/baixados.jpg?width=460&height=460',
     ),
   ];
 }
